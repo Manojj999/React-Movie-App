54c6f88e6dc634032cc5e84d9f9a8bcb23710a85
Manojj999 <jasoliyamanoj999@gmail.com>
2020-12-24 15:18:04 +0530

    Added Styled Component of Header file

--- src/components/elements/Header.jsx
+++ src/components/elements/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import RMDBLogo from "../images/reactMovie_logo.png";
+import TMDBLogo from "../images/tmdb_logo.svg";
+import { StyledHeader,StyledRMDBLogo,StyledTMDBLogo } from "../styles/StyledHeader";
+
+
+
 const Header = () => {
-    return(
-        <div>
-            <h1>Header Component</h1>
-        </div>
-    )
-}
+  return (
+    <StyledHeader>
+      <div className='header-content'>
+        <StyledRMDBLogo src={RMDBLogo} alt='rmdb logo' />
+        <StyledTMDBLogo src={TMDBLogo} alt='tmdb logo' />
+      </div>
+    </StyledHeader>
+  );
+};
 export default Header;
